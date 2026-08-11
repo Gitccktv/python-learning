7047810e6ab6c9c4b44643743725ebf6db34903e
ccktv <ccktv3738@163.com>
2026-08-11 14:11:36 +0800

    练习

---
+++ string.py
@@ -0,0 +1,13 @@
+str="python"
+print(str[0],"a"+str[1:4],str[-1],len(str))
+
+name=input("姓名：")
+age=int(input("年龄："))
+school=input("学校：")
+major=input("专业：")
+graduate_year=int(input("毕业年份："))
+
+print(f"""\
+我叫{name},今年{age}岁。
+是{graduate_year}年{school}{major}专业的毕业生。
+""")
---
+++ types.py
@@ -0,0 +1,8 @@
+num1=2
+num2=3.11
+str="agent"
+switch=True
+result=None
+
+print(type(num1),type(num2),type(str),type(switch),type(result))
+
