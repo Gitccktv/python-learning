f340fdf65e3f8dc1f468fa8efacd9118b90d9dc8
ccktv <ccktv3738@163.com>
2026-08-13 16:54:50 +0800

    day4

--- day4/conversation_analyzer/formatter.py
+++ day4/conversation_analyzer/formatter.py
@@ -1,9 +1,16 @@
+import logging
 def dict_output(result):
-    for key,value in result.items():
-        print(f"{key}: {value}")
+    try:
+        for key,value in result.items():
+            print(f"{key}: {value}")
+    except Exception as e:
+        logging.warning(f"发生错误：{e}")
     print()
 
 def list_output(result):
-    for i in range(len(result)):
-        print(f"{i+1}. {result[i]}")
+    try:
+        for i in range(len(result)):
+            print(f"{i+1}. {result[i]}")
+    except Exception as e:
+        logging.warning(f"发生错误：{e}")
     print()
